e90a1972cec9a25f4d0e063e5cf5a0bb591ab3cc
Ubuntu <ubuntu@ip-10-165-24-77.ec2.internal>
2013-07-07 01:58:46 +0000

    Updating it again

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = 'Hellow world 2';
+    var buffer = fs.readFileSync('index.html', encoding = 'utf8');
     response.send(buffer.toString(encoding = 'utf8'));
 });
 
